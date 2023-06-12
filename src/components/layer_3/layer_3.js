5d9f14e2b6ee4a8d9c841b85f44f22301c5ef1ee
KawanamiKazuki <kawanami.kazuki@outlook.com>
2023-06-12 22:08:11 +0200

    Updates main photos

--- src/components/layer_3/layer_3.js
+++ src/components/layer_3/layer_3.js
@@ -25,7 +25,7 @@ layer3Text.set_margin("0% 0% 0px 0px");
 layer3Text.set_padding("0% 0px 0px 0px");
 layer3Text.set_innerHTML("<p> Merci &agrave tous et &agrave toutes pour votre participation. Ce fut un grand bonheur de vous avoir parmis nous. <br><br>\
 Les photos du mariage sont disponibles ici:<br>\
-<a style=\"color:white\" href=https://1drv.ms/u/s!Ah3IXYPmz59HjXZ1THHwHhbWvoNE?e=HgQIq1>photos mariage (mise &agrave jour le 12/06/23).</a><br><br>\
+<a style=\"color:white\" href=https://1drv.ms/u/s!Ah3IXYPmz59HjXZ1THHwHhbWvoNE?e=HgQIq1 target='_blank'>photos mariage (mise &agrave jour le 12/06/23).</a><br><br>\
 Si vous m&ecircme avez des photos que vous souhaitez partager, merci de les envoyer &agrave Thomas &agrave l'adresse suivante: foucrier.t@hotmail.fr. Il pourra ainsi mettre &agrave jour le site pour y inclure vos photos (La date changera &agrave chaque mise &agrave jour pour indiquer les changements). </p>");
 layer3Text.set_fontFamily("Supermercado One");
 layer3Text.set_fontSize("250%");

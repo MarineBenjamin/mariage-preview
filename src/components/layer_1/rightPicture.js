86dd91de67bcf483831841d278c2ab4d815adfc5
KawanamiKazuki <kawanami.kazuki@outlook.com>
2023-06-12 23:14:45 +0200

    update link and main photos

--- src/components/layer_1/rightPicture.js
+++ src/components/layer_1/rightPicture.js
@@ -3,9 +3,9 @@ layer1RightPicture.set_id("layer1RightPicture");
 layer1RightPicture.set_position("absolute");
 layer1RightPicture.set_src(rpath + "assets/marinebenjamin_mariage2.jpg");
 layer1RightPicture.set_width("30%");
-layer1RightPicture.set_height("100%");
+layer1RightPicture.set_height("98%");
 
-layer1RightPicture.set_top("0px");
+layer1RightPicture.set_top("2%");
 layer1RightPicture.set_left("66%");
 layer1RightPicture.set_margin("0px 0px 0px 0px");
 layer1RightPicture.set_padding("0px 0px 0px 0px");
